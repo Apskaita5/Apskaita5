d93bbdfd3be606c4e1a569e5590396c3f89b5b91
Marius Dagys <Marius Dagys@MICROSOFT-PC.Home>
2015-09-14 15:19:33 +0300

    commit

---
+++ Apskaita5/Apskaita3/Apskaita/Workers/WageSheetItem.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+''' Atsiskaitymo lapelio printinimui.
+''' </summary>
+''' <remarks></remarks>
+Public Class WageSheetItem
+
+    Public ReadOnly Sheet As Workers.WageSheet
+    Public ReadOnly Item As Workers.WageItem
+
+    Public Sub New(ByVal currenSheet As Workers.WageSheet, ByVal currentItem As Workers.WageItem)
+        Sheet = currenSheet
+        Item = currentItem
+    End Sub
+
+End Class
